1eafaea8a095a1e4385bf5f2503fe9f883c3765c
Thomas Birke <tbirke@netextreme.de>
2013-04-10 21:50:34 +0200

    use sequelize for model handling

---
+++ controllers/service.js
@@ -0,0 +1,57 @@
+var Services = function Services() {};
+
+Services.setModel = function setModel(model) {
+  Services.model = model;
+}
+
+Services.getList = function getList(req, res, next) {
+  Services.model.findAll().success(function(services){res.send(services);});
+}
+
+Services.getOne = function getOne(req, res, next) {
+  Services.model.find(req.params.id).success(function(service){res.send(service);});
+}
+
+Services.create = function create(req, res, next) {
+  Services.model
+    .build({
+      'name':    req.body.name,
+      'alias':   req.body.alias,
+      'rate':    req.body.rate,
+      'user_id': userid
+    })
+    .save()
+    .success(function(newService) {res.send(newService);})
+    .error(function(error) {
+      console.log('could not create service: ' + error);
+    });
+}
+
+Services.update = function update(req, res, next) {
+  Services.model.find(req.params.id).success(function(service){
+    if ("undefined" != typeof(req.body.name)) {
+      service.name  = req.body.name;
+    }
+    if ("undefined" != typeof(req.body.alias)) {
+      service.alias = req.body.alias;
+    }
+    if ("undefined" != typeof(req.body.rate)) {
+      service.rate  = req.body.rate;
+    }
+
+    service
+      .save()
+      .success(function(updatedService) {res.send(updatedService);})
+      .error(function(error) {
+        console.log('could not create service: ' + error);
+      });
+  });
+}
+
+Services.del = function del(req, res, next) {
+  Services.model.find(req.params.id).success(function(service){
+    service.destroy().success(function() {res.send('');})
+  });
+}
+
+module.exports = Services;
--- server.js
+++ server.js
@@ -2,93 +2,27 @@
 
 var SERVER_PORT = 8001;
 
-var fs      = require('fs');
-var restify = require('restify');
-var jade    = require('jade');
-var mysql   = require('mysql');
-var sys     = require('sys');
-
-var Service = require('./services');
+var fs        = require('fs');
+var restify   = require('restify');
+var jade      = require('jade');
+var mysql     = require('mysql');
+var sys       = require('sys');
+var Sequelize = require("sequelize");
 
 // @TODO
 var userid = 1;
 
-function sendQueryResult(query, res, filter, singleResult, responseQuery)
-{
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    //password : '',
-    database : 'dime'
-  });
-  connection.connect();
-  connection.query(query, function(err, rows) {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    if ('function' == typeof(filter)) {
-      rows = filter(rows);
-    }
-    if (responseQuery) {
-      return sendQueryResult(
-        responseQuery.replace('{{last_insert_id}}', rows.insertId),
-        res,
-        filter,
-        singleResult
-      );
-    } else if (res) {
-      res.send(singleResult ? rows[0] : rows);
-    }
-  });
-  connection.end();
-}
+/* database connection */
+var sequelize = new Sequelize('dime', 'root', '');
 
-function getServiceList(req, res, next) {
-  sendQueryResult(
-    Service.getFindAllQuery(userid),
-    res,
-    null,
-    false
-  );
-}
+/* models */
+var Service = sequelize.import(__dirname + '/models/service');
 
-function getService(req, res, next) {
-  sendQueryResult(
-    Service.getFindOneByIdQuery(userid, req.params.id),
-    res,
-    null,
-    true
-  );
-}
+/* controllers */
+var services = require(__dirname + '/controllers/service');
+services.setModel(Service);
 
-function createService(req, res, next) {
-  sendQueryResult(
-    Service.getCreateQuery(userid, req.body),
-    res,
-    null,
-    true,
-    Service.getFindOneByIdQuery(userid, '{{last_insert_id}}')
-  );
-}
-
-function updateService(req, res, next) {
-  sendQueryResult(
-    Service.getUpdateQuery(userid, req.params.id, req.body),
-    res,
-    null,
-    true,
-    Service.getFindOneByIdQuery(userid, req.params.id)
-  );
-}
-
-function deleteService(req, res, next) {
-  sendQueryResult(
-    Service.getDeleteQuery(userid, req.params.id),
-    res
-  );
-}
-
+/* init server */
 var server = restify.createServer({
   name: 'dime',
   version: '0.1.0'
@@ -97,12 +31,14 @@ server.use(restify.acceptParser(server.acceptable));
     server.use(restify.queryParser());
     server.use(restify.bodyParser());
 
-server.get('/services', getServiceList);
-server.get('/services/:id', getService);
-server.post('/services', createService);
-server.put('/services/:id', updateService);
-server.del('/services/:id', deleteService);
+/* routes */
+server.get ('/services',     services.getList);
+server.get ('/services/:id', services.getOne);
+server.post('/services',     services.create);
+server.put ('/services/:id', services.update);
+server.del ('/services/:id', services.del);
 
+/* start server */
 server.listen(SERVER_PORT, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
