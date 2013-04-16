e6ac493de773cffe3bdda5f8516745e05e03e585
Thomas Birke <tbirke@netextreme.de>
2013-04-16 22:02:16 +0200

    fix datetime fields data type

--- models/timeslice.js
+++ models/timeslice.js
@@ -2,8 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Timeslice", {
     name:        DataTypes.STRING,
     activity_id: DataTypes.INTEGER,
-    started_at:  DataTypes.DATETIME,
-    stopped_at:  DataTypes.DATETIME,
+    started_at:  DataTypes.DATE,
+    stopped_at:  DataTypes.DATE,
     duration:    DataTypes.INTEGER
   }, {
     underscored: true,
