9d1ddf0c1abc4c582cde79d84a7147dcb9ed54d5
Thomas Birke <tbirke@netextreme.de>
2013-07-30 06:58:10 +0200

    set default value for duration

--- models.js
+++ models.js
@@ -58,7 +58,11 @@ var Timeslice = sequelize.define("Timeslice", {
   activity_id: Sequelize.INTEGER,
   started_at:  Sequelize.DATE,
   stopped_at:  Sequelize.DATE,
-  duration:    Sequelize.INTEGER
+    duration:    {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
 }, {
   underscored: true,
   tableName: 'timeslices'
