18a67b4551df56253fe5c7228ad19cffbfb6d2d9
Danilo Kuehn <dk@nogo-software.de>
2013-08-09 16:54:09 +0200

    fix authentication - Problem was the new Buffer(digest) should be new Buffer(digest, 'binary')

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -1,36 +1,26 @@
-var crypto    = require('crypto');
+var crypto = require('crypto');
 
 /* authentication */
 function authenticate(req, res, next) {
     var iterations = 5000;
-    var algorithm  = 'sha512';
+    var algorithm = 'sha512';
     var authz = req.authorization;
-    console.log('%j', authz)
 
-        console.log('fake authorization')
-        req.userid=1;
-        return next();
     if (authz.scheme === 'Basic') {
-      authenticate.User.find({ where: { username: authz.basic.username } }).success(function(user) {
-    /* @FIXME
-        var salted = authz.basic.password + '{' + user.salt + '}';
-        var digest = crypto.createHash(algorithm).update(salted).digest('binary');
-        for (var i=1; i<iterations; i++) {
-          digest = crypto.createHash(algorithm).update(digest + salted).digest('binary');
-        }
-        var base64 = new Buffer(digest).toString('base64');
-        console.log(authz.basic.username);
-        if (user.password !== base64) {
-          return next(new authenticate.Error(
-            '%s failed to authenticate',
-            authz.basic.username
-          ));
-        }
-    */
-        req.userid=1;
+        authenticate.User.find({ where: { username: authz.basic.username } }).success(function (user) {
+            var salted = authz.basic.password + '{' + user.salt + '}';
+            var digest = crypto.createHash(algorithm).update(salted).digest('binary');
+            for (var i = 1; i < iterations; i++) {
+                digest = crypto.createHash(algorithm).update(digest + salted).digest('binary');
+            }
+            var base64 = new Buffer(digest, 'binary').toString('base64');
+            if (user.password !== base64) {
+                return next(new authenticate.Error('%s failed to authenticate', authz.basic.username));
+            }
+            req.userid = 1;
 
-        return next();
-      });
+            return next();
+        });
     }
 }
 
